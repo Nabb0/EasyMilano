df8835af2b2b1475b18744dfe433fd32a28d8a06
Nabb0 <rivavalentino.f@iisgalvanimi.edu.it>
2022-04-28 22:11:09 +0000

    no

--- easymilano.py
+++ easymilano.py
@@ -1,61 +1,64 @@
-from flask import Flask,render_template, request, Response, redirect, url_for
-app = Flask(__name__)
-import csv
-import io
-import pandas as pd
-import geopandas as gpd
-import contextily
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
-import matplotlib
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import matplotlib
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+import contextily
+import geopandas as gpd
+import pandas as pd
+import io
+import csv
+from flask import Flask, render_template, request, Response, redirect, url_for
+app = Flask(__name__)
+matplotlib.use('Agg')
 
-#Dichiarazioni dei geodataframe
-dati = []
+# Dichiarazioni dei geodataframe
+dati = pd.read_json("/workspace/EasyMilano/static/file/dati.json")
 
-quartieri=gpd.read_file('/workspace/EasyMilano/static/file/ds964_nil_wm-20220405T093028Z-001.zip')
-mezzi_superficie=gpd.read_file('/workspace/EasyMilano/static/file/tpl_percorsi.geojson')
-uffici_postali=pd.read_csv('/workspace/EasyMilano/static/file/uffici_postali_milano.csv')
-civici=pd.read_csv('/workspace/EasyMilano/static/file/civici.csv')
-comandi_polizialocale=gpd.read_file('/workspace/EasyMilano/static/file/geocoded_comandi-decentrati-polizia-locale__final.geojson')
-scuole=pd.read_csv('/workspace/EasyMilano/static/file/CITTA_METROPOLITANA_MILANO_-_Scuole_di_ogni_ordine_e_grado.csv')
-metro=gpd.read_file('/workspace/EasyMilano/static/file/tpl_metropercorsi.geojson')
-stradario=pd.read_csv('/workspace/EasyMilano/static/file/stradario (2).csv')
+quartieri = gpd.read_file(
+    '/workspace/EasyMilano/static/file/ds964_nil_wm-20220405T093028Z-001.zip')
+mezzi_superficie = gpd.read_file(
+    '/workspace/EasyMilano/static/file/tpl_percorsi.geojson')
+uffici_postali = pd.read_csv(
+    '/workspace/EasyMilano/static/file/uffici_postali_milano.csv')
+civici = pd.read_csv('/workspace/EasyMilano/static/file/civici.csv')
+comandi_polizialocale = gpd.read_file(
+    '/workspace/EasyMilano/static/file/geocoded_comandi-decentrati-polizia-locale__final.geojson')
+scuole = pd.read_csv(
+    '/workspace/EasyMilano/static/file/CITTA_METROPOLITANA_MILANO_-_Scuole_di_ogni_ordine_e_grado.csv')
+metro = gpd.read_file(
+    '/workspace/EasyMilano/static/file/tpl_metropercorsi.geojson')
+stradario = pd.read_csv('/workspace/EasyMilano/static/file/stradario (2).csv')
 
-#home e registrazione
+# home e registrazione
+
 
 @app.route('/', methods=['GET'])
 def home():
     return render_template('home.html')
 
-@app.route('/register', methods=['GET','POST'])
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
-        return  render_template('register.html')
-    else: 
-        name = request.args['name']
-        surname = request.args['surname']
-        via = request.args['via']
-        pwd = request.args['pwd']
-        cpwd = request.args['cpwd']
-        
-    df1 = pd.read_csv('dati.csv')
-    
-    
-    nuovi_dati = {'name': name, 'surname': surname,'via' : via,'pwd': pwd}
-    
-    df1 = df1.append(nuovi_dati,ignore_index=True)
-    
-    
-    df1.to_csv('dati.csv', index=False)
-    rdf1 = df1.to_html()
+        return render_template('register.html')
+    elif request.method == 'POST':
+        name = request.form.get("name")
+        surname = request.form.get("surname")
+        psw = request.form.get("pwd")
+        cpsw = request.form.get("cpwd")
+        via = request.form.get("via")
+        if psw==cpsw:
+            dati.append({name: name,surname:surname,psw:psw,via:via},ignore_index=True)
+            dati.to_json("/workspace/EasyMilano/static/file/dati.json")
+            
+            return render_template('a.html')
+        else:
+            return "le password non corrispondono"
 
 
 
-#verifico se posso scrivere
-
+# verifico se posso scrivere
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=3245, debug=True)
+    app.run(host='0.0.0.0', port=3245, debug=True)
